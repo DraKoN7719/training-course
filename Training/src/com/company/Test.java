7364f4cadae7e53aaa562dc9820188bfc171048e
DraKoN7719 <Drakon7719@gmail.com>
2018-10-21 15:44:00 +0400

    add simple classes

---
+++ Training/src/com/company/Test.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Test {
+    int a; double b; String newName;
+
+    public int getA(){
+        return a;
+    }
+    public void setA(int a){
+        this.a = a;
+    }
+    public double getB(){
+        return b;
+    }
+    public void setB(double b){
+        this.b = b + 50;
+    }
+    public String getNewName(){
+        return newName;
+    }
+    public void setNewName(String newName){
+        this.newName = newName;
+    }
+}
